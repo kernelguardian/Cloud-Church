7a183c8b02e52de631552783579215d3a394faee
Asish Binu Mathew <asishbm98@gmail.com>
2019-01-07 05:05:10 +0530

    It's 5 in the morning,finished adding Login & Signup page

---
+++ app/src/main/java/com/example/asish/cloudchurch/loginIntent.java
@@ -0,0 +1,13 @@
+package com.example.asish.cloudchurch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class loginIntent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_intent);
+    }
+}
